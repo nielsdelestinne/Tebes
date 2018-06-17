4a1c65dbcaf18929a202e7f4b0f858ca15277fea
nielsdelestinne <nielsdelestinne@gmail.com>
2018-06-17 14:50:16 +0200

    sign-in screen added

--- tebes-frontend/src/app/app.component.ts
+++ tebes-frontend/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "./User";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  public user: User;
+  private hasEagerlyClickedPlay: boolean;
+  private hasClickedPlay: boolean;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.user = new User('');
+  }
+
+  onPlay(): void {
+      if(this.hasValidUsername()) {
+        console.log("jup");
+        this.hasClickedPlay = true;
+      } else {
+        this.hasEagerlyClickedPlay = true;
+      }
+  }
+
+  hasValidUsername(): boolean {
+    return this.user.username.trim() != ''
+      && this.user.username.trim() != null
+      && this.user.username.trim() != undefined;
+  }
+
+  hasEagerlyClickedPlayButton(): boolean {
+    return this.hasEagerlyClickedPlay;
+  }
+
+  hasClickedPlayButton(): boolean {
+    return this.hasClickedPlay;
+  }
 }
